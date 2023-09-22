f2f528e832ab4f09ef4ce91720c10237f2ac1bba
Kadar Admankar <admankarkadar@sggs.ac.in>
2023-09-22 23:45:10 +0530

    get all users route created

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ import express from 'express'
 const app = express()
 import connectDB from './DB/connectDB.js';
 
+import router from './routes/userRooutes.js';
 connectDB(process.env.DB_URL);
-
-app.use('/api', (req, res, next)=>{
-     res.send('Hello World')
-})
+// app.use('/api', (req, res, next)=>{
+//      res.send('Hello World')
+// })
+app.use('/api/user', router)
 
 app.listen(5000, ()=>{
     console.log('server started')
---
+++ models/User.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name:{ type:String, required:true },
+    email:{ type:String, required:true, unique:true },
+    password:{ type:String, required:true, minLength:6 }
+})
+
+export default mongoose.model("User", UserSchema)
