d72621d0ae9b9ea52c14e68b460e2e6006500c38
Kadar Admankar <admankarkadar@sggs.ac.in>
2023-09-24 23:30:15 +0530

    login api created

--- routes/userRooutes.js
+++ routes/userRooutes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { getAllUser, signUp } from "../controllers/userControoller.js"
+import { getAllUser, login, signUp } from "../controllers/userController.js"
 
 const router = express.Router()
 
 router.get('/', getAllUser)
 router.post('/signUp', signUp)
+router.post('/login', login)
 export default router
