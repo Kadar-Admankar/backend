4e219730d8f4c5af780316317bdc9d5058b3054a
Kadar Admankar <admankarkadar@sggs.ac.in>
2023-09-24 23:59:30 +0530

    blog schema created

---
+++ models/Blog.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+const blogSchema = new mongoose.Schema({
+    title: { type:String, required: true },
+    description: { type:String, required: true },
+    image: { type:String, required: true },
+    user: { type:String, required: true },
+})
+
+export default mongoose.model("Blog", blogSchema)
